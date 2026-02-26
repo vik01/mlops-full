cd31a1584c51e6487e91a605c241b06441244f7a
matteokhoueiri <matteokhoueiri@student.ie.edu>
2026-02-26 14:10:08 +0100

    Added new branch for logistic regression.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 """
-Module: Main Pipeline
+Module: Main Pipeline Blah
 ---------------------
 Role: Orchestrate the entire flow (Load -> Clean -> Validate -> Train -> Evaluate).
 Usage: python -m src.main
